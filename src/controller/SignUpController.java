774b7c9ac4a09bc7dc0e200157762bbed47ada10
prabhatkumar9 <prabhatvisvakarma9@gmail.com>
2020-04-08 12:55:50 +0100

    completed

--- src/controller/SignUpController.java
+++ src/controller/SignUpController.java
@@ -2,8 +2,6 @@ package controller;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.time.LocalDate;
-import java.time.LocalDateTime;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -29,6 +27,19 @@ public class SignUpController extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Sign up Controller");
 		// Fill your code here
+		Connection con = null;
+		ConnectionManager cm = new ConnectionManager();
+		try {
+			con = cm.getConnection();
+			if(con!=null)
+				System.out.println("Connection Established");
+			else
+				System.out.println("Check your Connection");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		RequestDispatcher rd=this.getServletContext().getRequestDispatcher("/WEB-INF/views/signupView.jsp");
 		rd.forward(request,response);
 	}
